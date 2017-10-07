d830dcf194fc758d6f6aeb27923f308d9ab5e7bc
Lucas Berg <berg@ice.ufjf.br>
2017-10-07 08:37:34 -0300

    Add Meshes Exp-2

--- Mesh_Generator/genMeshes.sh
+++ Mesh_Generator/genMeshes.sh
@@ -7,23 +7,42 @@ fi
 SK_FOLDER="Skeletons"
 MSH_FOLDER="Meshes"
 
-# Bifurcation Iterative
-./meshGenerator $SK_FOLDER/biffIter05cm.vtk $MSH_FOLDER/biffIter05cm-alien.msh --alien
-./meshGenerator $SK_FOLDER/biffIter05cm.vtk $MSH_FOLDER/biffIter05cm-dog.msh --dog
-./meshGenerator $SK_FOLDER/biffIter05cm.vtk $MSH_FOLDER/biffIter05cm-orc.msh --orc
-./meshGenerator $SK_FOLDER/biffIter05cm.vtk $MSH_FOLDER/biffIter05cm-pig.msh --pig
+./meshGenerator $SK_FOLDER/cable2-05cm.vtk $MSH_FOLDER/cable2-05cm-alien.msh --alien
+./meshGenerator $SK_FOLDER/cable2-05cm.vtk $MSH_FOLDER/cable2-05cm-dog.msh --dog
+./meshGenerator $SK_FOLDER/cable2-05cm.vtk $MSH_FOLDER/cable2-05cm-orc.msh --orc
+./meshGenerator $SK_FOLDER/cable2-05cm.vtk $MSH_FOLDER/cable2-05cm-pig.msh --pig
 
-./meshGenerator $SK_FOLDER/biffIter1cm.vtk $MSH_FOLDER/biffIter1cm-alien.msh --alien
-./meshGenerator $SK_FOLDER/biffIter1cm.vtk $MSH_FOLDER/biffIter1cm-dog.msh --dog
-./meshGenerator $SK_FOLDER/biffIter1cm.vtk $MSH_FOLDER/biffIter1cm-orc.msh --orc
-./meshGenerator $SK_FOLDER/biffIter1cm.vtk $MSH_FOLDER/biffIter1cm-pig.msh --pig
+./meshGenerator $SK_FOLDER/cable2-1cm.vtk $MSH_FOLDER/cable2-1cm-alien.msh --alien
+./meshGenerator $SK_FOLDER/cable2-1cm.vtk $MSH_FOLDER/cable2-1cm-dog.msh --dog
+./meshGenerator $SK_FOLDER/cable2-1cm.vtk $MSH_FOLDER/cable2-1cm-orc.msh --orc
+./meshGenerator $SK_FOLDER/cable2-1cm.vtk $MSH_FOLDER/cable2-1cm-pig.msh --pig
 
-./meshGenerator $SK_FOLDER/biffIter2cm.vtk $MSH_FOLDER/biffIter2cm-alien.msh --alien
-./meshGenerator $SK_FOLDER/biffIter2cm.vtk $MSH_FOLDER/biffIter2cm-dog.msh --dog
-./meshGenerator $SK_FOLDER/biffIter2cm.vtk $MSH_FOLDER/biffIter2cm-orc.msh --orc
-./meshGenerator $SK_FOLDER/biffIter2cm.vtk $MSH_FOLDER/biffIter2cm-pig.msh --pig
+./meshGenerator $SK_FOLDER/cable2-2cm.vtk $MSH_FOLDER/cable2-2cm-alien.msh --alien
+./meshGenerator $SK_FOLDER/cable2-2cm.vtk $MSH_FOLDER/cable2-2cm-dog.msh --dog
+./meshGenerator $SK_FOLDER/cable2-2cm.vtk $MSH_FOLDER/cable2-2cm-orc.msh --orc
+./meshGenerator $SK_FOLDER/cable2-2cm.vtk $MSH_FOLDER/cable2-2cm-pig.msh --pig
 
-./meshGenerator $SK_FOLDER/biffIter5cm.vtk $MSH_FOLDER/biffIter5cm-alien.msh --alien
-./meshGenerator $SK_FOLDER/biffIter5cm.vtk $MSH_FOLDER/biffIter5cm-dog.msh --dog
-./meshGenerator $SK_FOLDER/biffIter5cm.vtk $MSH_FOLDER/biffIter5cm-orc.msh --orc
-./meshGenerator $SK_FOLDER/biffIter5cm.vtk $MSH_FOLDER/biffIter5cm-pig.msh --pig
+./meshGenerator $SK_FOLDER/cable2-5cm.vtk $MSH_FOLDER/cable2-5cm-alien.msh --alien
+./meshGenerator $SK_FOLDER/cable2-5cm.vtk $MSH_FOLDER/cable2-5cm-dog.msh --dog
+./meshGenerator $SK_FOLDER/cable2-5cm.vtk $MSH_FOLDER/cable2-5cm-orc.msh --orc
+./meshGenerator $SK_FOLDER/cable2-5cm.vtk $MSH_FOLDER/cable2-5cm-pig.msh --pig
+
+./meshGenerator $SK_FOLDER/biff2-05cm.vtk $MSH_FOLDER/biff2-05cm-alien.msh --alien
+./meshGenerator $SK_FOLDER/biff2-05cm.vtk $MSH_FOLDER/biff2-05cm-dog.msh --dog
+./meshGenerator $SK_FOLDER/biff2-05cm.vtk $MSH_FOLDER/biff2-05cm-orc.msh --orc
+./meshGenerator $SK_FOLDER/biff2-05cm.vtk $MSH_FOLDER/biff2-05cm-pig.msh --pig
+
+./meshGenerator $SK_FOLDER/biff2-1cm.vtk $MSH_FOLDER/biff2-1cm-alien.msh --alien
+./meshGenerator $SK_FOLDER/biff2-1cm.vtk $MSH_FOLDER/biff2-1cm-dog.msh --dog
+./meshGenerator $SK_FOLDER/biff2-1cm.vtk $MSH_FOLDER/biff2-1cm-orc.msh --orc
+./meshGenerator $SK_FOLDER/biff2-1cm.vtk $MSH_FOLDER/biff2-1cm-pig.msh --pig
+
+./meshGenerator $SK_FOLDER/biff2-2cm.vtk $MSH_FOLDER/biff2-2cm-alien.msh --alien
+./meshGenerator $SK_FOLDER/biff2-2cm.vtk $MSH_FOLDER/biff2-2cm-dog.msh --dog
+./meshGenerator $SK_FOLDER/biff2-2cm.vtk $MSH_FOLDER/biff2-2cm-orc.msh --orc
+./meshGenerator $SK_FOLDER/biff2-2cm.vtk $MSH_FOLDER/biff2-2cm-pig.msh --pig
+
+./meshGenerator $SK_FOLDER/biff2-5cm.vtk $MSH_FOLDER/biff2-5cm-alien.msh --alien
+./meshGenerator $SK_FOLDER/biff2-5cm.vtk $MSH_FOLDER/biff2-5cm-dog.msh --dog
+./meshGenerator $SK_FOLDER/biff2-5cm.vtk $MSH_FOLDER/biff2-5cm-orc.msh --orc
+./meshGenerator $SK_FOLDER/biff2-5cm.vtk $MSH_FOLDER/biff2-5cm-pig.msh --pig
