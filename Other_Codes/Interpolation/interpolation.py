ba585ae4b4a62a5eb5852117340c789c803fff55
bergolho <berg@ice.ufjf.br>
2017-10-22 23:40:08 -0200

    Add Interpolation

--- Other_Codes/Interpolation/interpolation.py
+++ Other_Codes/Interpolation/interpolation.py
@@ -2,16 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 
-# Points to interpolate
-x = np.linspace(0,10,num=11,endpoint=True)
-y = np.cos(-x**2 / 9.0)
+# Read points from file
+x = []
+y = []
+file = open("points.dat","r")
+for line in file:
+    token = line.split()
+    x.append(float(token[0]))
+    y.append(float(token[1]))
 
 # Interpolation function
 f = interp1d(x,y)
-f2 = interp1d(x,y,kind='cubic')
+#f2 = interp1d(x,y,kind='cubic')
 
-# Plot the interpolation
-xnew = np.linspace(0,10,num=41,endpoint=True)
-plt.plot(x,y,'o',xnew,f(xnew),'-',xnew,f2(xnew),'--')
-plt.legend(['data','linear','cubic'], loc='best')
+xnew = np.linspace(min(x),max(x),num=41,endpoint=True)
+plt.plot(x,y,'o',xnew,f(xnew),'-')
+plt.legend(['data','linear'], loc='best')
+plt.grid()
 plt.show()
