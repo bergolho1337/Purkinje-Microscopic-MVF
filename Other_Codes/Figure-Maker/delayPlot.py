afda0f2eed6298eeec6e23c45876857b3d61dd5a
Lucas Berg <berg@ice.ufjf.br>
2017-11-19 11:39:54 -0200

    Add Figure-Maker Potential Plot

--- Other_Codes/Figure-Maker/delayPlot.py
+++ Other_Codes/Figure-Maker/delayPlot.py
@@ -21,8 +21,28 @@ def plotDelay ():
 	pyplot.legend()
 	pyplot.savefig("delay-LiRudy.pdf")
 
-def main ():    	
-	plotDelay()
+def plotAP ():
+	filename = "noble59.dat"
+	data = genfromtxt(filename)
+	pyplot.plot(data[:,0], data[:,1], label = "Vm", linestyle ="-", color="k", linewidth=2)
+	pyplot.tick_params(
+				    axis='x',          # changes apply to the x-axis
+				    which='both',      # both major and minor ticks are affected
+				    bottom='off',      # ticks along the bottom edge are off
+				    top='off',         # ticks along the top edge are off
+				    labelbottom='off') # labels along the bottom edge are off
+	pyplot.yticks(size=18)
+	pyplot.xlim([400,900])
+	pyplot.ylim([-90,50])
+	pyplot.title("Potencial de Acao")
+	#pyplot.xlabel("Tempo (ms)")
+	pyplot.ylabel("Potencial Transmembranico (mV)", size=15)
+	pyplot.legend()
+	pyplot.savefig("AP.png")
+
+def main ():
+	plotAP()    	
+	#plotDelay()
 
 if __name__ == "__main__":
     main()
